c0377b64f191365c0bfc616fbba08a4545ed69d5
Ákos <akos.paska@risingstack.com>
2022-04-04 14:36:12 +0200

    das ist meine ok?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = "DAS IS MEIN TITLE GEIL?!";
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
